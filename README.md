9ef47a77e0ff88a62a6d30a94772fd5c1333619d
Michael Hodge <michael.hodge@ons.gov.uk>
2020-05-24 14:05:54 +0100

    Fixes #2

--- README.md
+++ README.md
@@ -1,36 +1,52 @@
 # changing-rooms
 
-A simple python script that crawls [www.worldfootball.net](www.worldfootball.net) to find squad mates of a requested player.
+A simple python script that crawls [www.worldfootball.net](www.worldfootball.net) to find squad mates of a requested player (both club and national (senior)).
 
-### Requires
+## Step-by-step Guide
+
+### 1. Install Python
+
+You will need Python 3. The easiest way is to use Anaconda. Here is the [official installation guide](https://docs.anaconda.com/anaconda/install/).
+
+
+### 2. Clone this code
+
+Type `cd <path>` in your command line, where `<path>` is your chosen folder.
+
+Use `git clone https://github.com/mshodge/changing-rooms.git` in your command line to clone it locally to the folder.
+
+Type `cd changing-rooms` to enter the folder in your command line.
+
+### 3. Install Dependencies
+
+The code requires the following packages:
 
 - beautifulsoup4
 - requests
 - pandas
 - unidecode
 
-### Basic run
+To install run `conda install -r requirements.txt` in the `changing-rooms` folder.
+
+### 4. Basic Code Example
 
 Type `python changing-rooms.py -p <player_name>` into your terminal. Default player is the Fulham legend `Steed Malbranque`.
 
-### Examples
+As it runs it will print some information to the command line.
 
-See `/examples` for CSV files for a number of world famous players including Steed Malbranque (see table example below).
+A comma separated (CSV) file will be saved to the `changing-rooms` folder. You can open this in Excel now.
 
-|player|team                         |season|
-|------|-----------------------------|------|
-|Christophe Breton|Olympique Lyon               |1997-1998|
-|Gregory Coupet|Olympique Lyon               |1997-1998|
-|Ghislain Anselmini|Olympique Lyon               |1997-1998|
-|Jacek Bak|Olympique Lyon               |1997-1998|
-|Olivier Bellisi|Olympique Lyon               |1997-1998|
-|Patrice Carteron|Olympique Lyon               |1997-1998|
+The CSV file will have the following headers:
 
+- player (the name of the squad mate)
+- team (the team)
+- season (the season, e.g. 2000-2001)
+- type (club or nation)
 
-### Rules
+## Provided Examples
+
+See `/examples` for CSV files for a number of world famous players including Steed Malbranque.
+
+## Rules
 
 Please use responsibly on [www.worldfootball.net](www.worldfootball.net). They provide a brilliant free service, don't exploit that.
-
-### To do
-
-- Add national teams.
--- changing-rooms.py
+++ changing-rooms.py
@@ -7,6 +7,7 @@ import unidecode
 import re
 
 
+'''Args'''
 def get_args(command_line_arguments):
 	parser = argparse.ArgumentParser(description="find club and international squad players of a certain football "
 												 "player",
@@ -22,6 +23,25 @@ def get_args(command_line_arguments):
 	return args
 
 
+'''Find National Team URL'''
+def find_national_team_url(args, nation):
+
+	player_url = args.player.lower().replace(' ', '-')
+	international_history = f'https://www.worldfootball.net/player_summary/{player_url}/4/'
+	page = requests.get(international_history)
+	soup = BeautifulSoup(page.content, 'html.parser')
+
+	paths = soup.findAll('a', href=True, text=re.compile(nation))
+
+	for path in paths:
+		if path.attrs.get('href')[0:5] == '/team':
+			nation_url = path.attrs.get('href')
+			break
+
+	return nation_url
+
+
+'''Find Player's Club History'''
 def player_club_history(args):
 
 	player_url = args.player.lower().replace(' ','-')
@@ -34,16 +54,27 @@ def player_club_history(args):
 	years_start = []
 	years_end = []
 
+	still_playing = False
+
+	'''Finds current team if the player is still playing'''
 	try:
 		current_team = soup.findAll("td", {"width": "60%"})[0].findAll("a")
 		if current_team[0].string[0] != '1' and current_team[0].string[0] != '2':
 			teams.append(str(current_team[0].string))
 			teams_url.append(current_team[0].attrs.get('href'))
+			still_playing = True
 	except IndexError:
 		None
 
-	nationality = str(soup.findAll("span", {"itemprop": "nationality"})[0].string)
 
+	'''Finds nation of player'''
+	try:
+		nation = str(soup.findAll("span", {"itemprop": "nationality"})[0].string)
+	except IndexError:
+		print('\nAborting: Player not in database. Please check name is spelt correctly\n')
+		sys.exit()
+
+	'''Gets career years'''
 	years_table = soup.findAll("td", {"width": "20%"})
 	for year_table in years_table:
 		try:
@@ -53,8 +84,7 @@ def player_club_history(args):
 		except (ValueError, TypeError):
 			continue
 
-
-
+	'''Finds teams'''
 	teams_table = soup.findAll("td", {"width": "40%"})
 	for team_table in teams_table:
 		if str(team_table.string)[0] != '\n':
@@ -66,21 +96,62 @@ def player_club_history(args):
 	years_start = years_start[::-1]
 	years_end = years_end[::-1]
 
-	years_start = years_start + [years_end[-1]]
-	years_end = years_end + [2020]
+	years_start = years_start + [years_end[len(years_end) - 1]]
+	years_end = years_end + [(years_end[len(years_end) - 1] + 1)]
 
-	return teams, teams_url, years_start, years_end
+	if still_playing == True:
+		years_end[-1] = 2020
 
-def find_club_mates(args, teams, teams_url, years_start, years_end):
+	return teams, teams_url, years_start, years_end, nation
+
+
+'''Finds national squad mates of the player'''
+def find_national_mates(args, nation, nation_url, years_start, years_end):
+
 	player = []
 	club = []
 	season = []
+	type = []
+
+	print('\n----Retrieving National Team Information-----\n')
+
+	for i in range(years_start[0] + 1, years_end[-1] + 1):
+		print(f'Getting national squad mates for {args.player} for {nation} during the {i-1}-{i} season')
+		squad_url = f'https://www.worldfootball.net{nation_url}freundschaft-{i}/2/'
+		page = requests.get(squad_url)
+		soup = BeautifulSoup(page.content, 'html.parser')
+		players_table = soup.findAll("a")
+		for player_table in players_table:
+			try:
+				if str(player_table.attrs.get('href')[0:3]) == '/pl':
+					player_found = str(player_table.attrs.get('title'))
+					player_found = unidecode.unidecode(player_found)
+					player.append(player_found)
+					club.append(nation)
+					season.append(f'{i-1}-{i}')
+					type.append('nation')
+			except TypeError:
+				continue
+
+	return player, club, season, type
+
+
+'''Finds club mates of the player'''
+def find_club_mates(args, teams, teams_url, years_start, years_end):
+
+	player = []
+	club = []
+	season = []
+	type = []
+
+	print('\n----Retrieving Club Team Information-----\n')
 
 	for team in teams:
+
 		idx = teams.index(team)
-		players_tmp = []
+
 		for i in range(years_start[idx] + 1, years_end[idx] + 1):
-			print(f'Getting squad mates for {args.player} for {teams[idx]} during the {i-1}-{i} season')
+			print(f'Getting club squad mates for {args.player} for {teams[idx]} during the {i-1}-{i} season')
 			squad_url = f'https://www.worldfootball.net{teams_url[idx]}{i}/2/'
 			page = requests.get(squad_url)
 			soup = BeautifulSoup(page.content, 'html.parser')
@@ -93,31 +164,45 @@ def find_club_mates(args, teams, teams_url, years_start, years_end):
 						player.append(player_found)
 						club.append(team)
 						season.append(f'{i-1}-{i}')
+						type.append('club')
 				except TypeError:
 					continue
 
-	return player, club, season
+		'''Removes club incase the player has played for the same club more than once at two points in their career'''
+		teams[idx] = 'Searched'
+
+	return player, club, season, type
+
 
 def print_summary(args, player, team, season):
 	print(f' \n{args.player}:\n\n '
-		  f'- Has played for {len(set(team))} clubs \n'
-		  f' - Has had {len(set(player))} different squad mates \n'
+		  f'- Has played for {len(set(team)) - 1} clubs \n'
+		  f' - Has had {len(set(player)) - 1} different squad mates \n'
 		  f' - Has played across {len(set(season))} seasons \n')
 
-def save_results(args, player, team, season):
-	df = pd.DataFrame({'player': player, 'team': team, 'season': season})
+
+def save_results(args, player, team, season, type):
+	df = pd.DataFrame({'player': player, 'team': team, 'season': season, 'type': type})
 	df = df.drop_duplicates()
+	df = df[df.player != args.player]
 	player_name_save = re.sub('[^A-Za-z0-9]+', '', args.player).lower()
 	df.to_csv(f'{player_name_save}-clubmates.csv', index=False)
 
 
-
 def main(supplied_args):
 	args = get_args(supplied_args)
-	teams, teams_url, years_start, years_end = player_club_history(args)
-	player, team, season = find_club_mates(args, teams, teams_url, years_start, years_end)
+	teams, teams_url, years_start, years_end, nation = player_club_history(args)
+	nation_url = find_national_team_url(args, nation)
+	club_player, club_team, club_season, club_type = find_club_mates(args, teams, teams_url, years_start, years_end)
+	nation_player, nation_team, nation_season, nation_type = find_national_mates(args, nation, nation_url, years_start, years_end)
+
+	player = club_player + nation_player
+	team = club_team + nation_team
+	season = club_season + nation_season
+	type = club_type + nation_type
+
 	print_summary(args, player, team, season)
-	save_results(args, player, team, season)
+	save_results(args, player, team, season, type)
 
 
 if __name__ == '__main__':
